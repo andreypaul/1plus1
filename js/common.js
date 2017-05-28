f7cb1795336e157d8d74032082a29e8fae85574f
andreypaul <andrey-polyakov@mail.ru>
2017-05-29 00:17:31 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,14 @@
+
+$('.slider').slick(
+    {
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    }
+);
+
+$('.slider2').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
